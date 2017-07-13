017cc59fcd48b5fe31db7bcb57cd724e8756e1c4
walkerman <binguo@sohu-inc.com>
2017-07-13 15:59:48 +0800

    1.整理方法交换 2.完成运行时的方法交换的抽象方法

--- OCDeepLearning/NSObject+Runtime.m
+++ OCDeepLearning/NSObject+Runtime.m
@@ -1,96 +1,78 @@
-////
-////  NSObject+Runtime.m
-////  OCDeepLearning
-////
-////  Created by walker彬 on 2017/7/7.
-////  Copyright © 2017年 walker彬. All rights reserved.
-////
 //
-//#import "NSObject+Runtime.h"
-//#import <objc/runtime.h>
-//#import "ViewController.h"
+//  NSObject+Runtime.m
+//  OCDeepLearning
 //
-//@implementation NSObject (Runtime)
+//  Created by walker彬 on 2017/7/7.
+//  Copyright © 2017年 walker彬. All rights reserved.
 //
-////+ (void)load {
-////    Method m1 = class_getInstanceMethod([ViewController class], @selector(changeBackgroundColor));
-////    Method m2 = class_getInstanceMethod([ViewController class], @selector(exchangeMethod));
-////    
-////    method_exchangeImplementations(m1, m2);
-////}
-//
-//+ (void)load {
-//    static dispatch_once_t onceToken;
-//    dispatch_once(&onceToken, ^{
-//        Class class = [self class];
-//        
-//        SEL originalSelector = @selector(viewWillAppear:);
-//        SEL swizzledSelector = @selector(xxx_viewWillAppear:);
-//        
-//        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-//        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-//        
-//        // When swizzling a class method, use the following:
-//        // Class class = object_getClass((id)self);
-//        // ...
-//        // Method originalMethod = class_getClassMethod(class, originalSelector);
-//        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
-//        
-//        BOOL didAddMethod =
-//        class_addMethod(class,
-//                        originalSelector,
-//                        method_getImplementation(swizzledMethod),
-//                        method_getTypeEncoding(swizzledMethod));
-//        
-//        if (didAddMethod) {
-//            class_replaceMethod(class,
-//                                swizzledSelector,
-//                                method_getImplementation(originalMethod),
-//                                method_getTypeEncoding(originalMethod));
-//        } else {
-//            method_exchangeImplementations(originalMethod, swizzledMethod);
-//        }
-//    });
-//}
-//
-//#pragma mark - Method Swizzling
-//
-//- (void)xxx_viewWillAppear:(BOOL)animated {
-//    [self xxx_viewWillAppear:animated];
-//    NSLog(@"viewWillAppear: %@", self);
-//}
-//
-////- (void)exchangeInstanceMethod:(SEL)originalSelector SwizzledMethod:(SEL)swizzledMethod {
-////    static dispatch_once_t onceToken;
-////    dispatch_once(&onceToken, ^{
-////        Class class = [self class];
-////        
-////        SEL originalSelector = @selector(originalSelector);
-////        SEL swizzledSelector = @selector(swizzledMethod);
-////        
-////        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-////        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-////        
-////        // When swizzling a class method, use the following:
-////        // Class class = object_getClass((id)self);
-////        // ...
-////        // Method originalMethod = class_getClassMethod(class, originalSelector);
-////        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
-////        
-////        BOOL didAddMethod =
-////        class_addMethod(class,
-////                        originalSelector,
-////                        method_getImplementation(swizzledMethod),
-////                        method_getTypeEncoding(swizzledMethod));
-////        
-////        if (didAddMethod) {
-////            class_replaceMethod(class,
-////                                swizzledSelector,
-////                                method_getImplementation(originalMethod),
-////                                method_getTypeEncoding(originalMethod));
-////        } else {
-////            method_exchangeImplementations(originalMethod, swizzledMethod);
-////        }
-////    });
-////}
-//@end
+
+#import "NSObject+Runtime.h"
+#import <objc/runtime.h>
+#import "ViewController.h"
+
+@implementation NSObject (Runtime)
+
++ (void)load {
+    [[self class] exchangeInstanceMethod:@selector(viewDidAppear:) SwizzledMethod:@selector(xxx_viewWillAppear:)];
+}
+
+#pragma mark - Method Swizzling
+// 交换方法
++ (void)exchangeInstanceMethod:(SEL)originalSelector SwizzledMethod:(SEL)swizzledSelector {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        ///判断是否添加成功
+        //成功则拦截，失败则交换
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+        
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+// MARK: 实现方法交换和拦截
++ (void)exchangeInstanceMethod:(SEL)originalSelector SwizzledMethod:(SEL)swizzledSelector InClass: (Class)class {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        ///判断是否添加成功
+        //成功则拦截，失败则交换
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+
+
+// MARK: swizzledSelector
+- (void)xxx_viewWillAppear:(BOOL)animated {
+    [self xxx_viewWillAppear:animated];
+    NSLog(@"viewWillAppear: %@", self);
+}
+@end
--- OCDeepLearning/ViewController+SwizzlingMethod.m
+++ OCDeepLearning/ViewController+SwizzlingMethod.m
@@ -11,19 +11,19 @@
 
 @implementation ViewController (SwizzlingMethod)
 
-+ (void)load {
-    Method m1 = class_getInstanceMethod([ViewController class], @selector(viewDidAppear:));
-    Method m2 = class_getInstanceMethod([ViewController class], @selector(gb_viewDidAppear:));
-
-//    IMP p1 = method_getImplementation(m1);
-//    IMP p2 = method_getImplementation(m2);
-//    NSLog(@"Imp分别是%p,/n %p",p1,p2);
-    ///p1和m1对应的是两个地址
-
-    NSLog(@"method分别是%p,/n %p",m1,m2);
-
-    method_exchangeImplementations(m1, m2);
-}
+//+ (void)load {
+//    Method m1 = class_getInstanceMethod([ViewController class], @selector(viewDidAppear:));
+//    Method m2 = class_getInstanceMethod([ViewController class], @selector(gb_viewDidAppear:));
+//
+////    IMP p1 = method_getImplementation(m1);
+////    IMP p2 = method_getImplementation(m2);
+////    NSLog(@"Imp分别是%p,/n %p",p1,p2);
+//    ///p1和m1对应的是两个地址
+//
+//    NSLog(@"method分别是%p,/n %p",m1,m2);
+//
+//    method_exchangeImplementations(m1, m2);
+//}
 
 - (void)gb_viewDidAppear:(BOOL)animated {
     [self gb_viewDidAppear:YES];
