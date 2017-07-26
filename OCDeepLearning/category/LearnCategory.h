8ac5dba16a261b631aed6ca055bfa40957deaac0
walkerman <bin5211bin@gmail.com>
2017-07-26 18:50:46 +0800

    1. 增加了category的redeme文档 2. 扩展了使用类中的方法，取代分类中的方法 3. 增加了所有的方法列表的类方法

---
+++ OCDeepLearning/category/LearnCategory.h
@@ -0,0 +1,16 @@
+//
+//  LearnCategory.h
+//  OCDeepLearning
+//
+//  Created by walker彬 on 2017/7/26.
+//  Copyright © 2017年 walker彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LearnCategory : NSObject
+
+@property(nonatomic, strong) NSString *name;
+
+- (void)testCategory;
+@end
