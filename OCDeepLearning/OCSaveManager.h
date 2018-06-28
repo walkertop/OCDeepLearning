1c45d918cd2df3483646ee586035346bb332fb19
walker <bin5211bin@gmail.com>
2018-06-28 11:48:30 +0800

    【ADD】增加一个专门的管理类OCSaveManager，来处理数据保存

---
+++ OCDeepLearning/OCSaveManager.h
@@ -0,0 +1,15 @@
+//
+//  OCSaveManager.h
+//  OCDeepLearning
+//
+//  Created by walker on 2018/6/28.
+//  Copyright © 2018年 walker彬. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSaveManager : NSObject
+
+- (instancetype)sharedInstance;
+
+@end
